7c90549770dd0f974e6aea453289f7b369fd7d1f
Akshay Belhe <abelhe8900@gmail.com>
2024-05-12 23:29:45 -0500

    change to env variable

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,13 +6,14 @@ const connectDB = async () => {
             // can y see?, which part do i change? after .net/put a db name // like that?
             // Now your db will be changed and new db will not have any data, so start testing again and check this db if
             // data is being saved or not....ok will do u do the testing let me see your other code
-            await mongoose.connect('mongodb+srv://akshay:ankitlovesfood@meals.il3tlrl.mongodb.net/when2eat?retryWrites=true&w=majority&appName=meals');
+            // await mongoose.connect('mongodb+srv://akshay:ankitlovesfood@meals.il3tlrl.mongodb.net/when2eat?retryWrites=true&w=majority&appName=meals');
             // console.log(`MongoDB Connected`);
+            await mongoose.connect(process.env.MONGO_URL)
             // set debug to true to see the operations on the database
             mongoose.set('debug', true);
         } catch (error) {
-            // console.error(`Error: ${(error as Error).message}`);
-            process.exit(1);
+            console.error(`Error: ${(error as Error).message}`);
+            // process.exit(1);
         }
     };
 
