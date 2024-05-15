0a7c0f4951ee8e20a3da95bbb532fc3f77d0ead3
Akshay Belhe <abelhe8900@gmail.com>
2024-05-15 13:05:01 -0500

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react"
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 import Layout from "./layout"
 import type { AppProps } from "next/app";
 
